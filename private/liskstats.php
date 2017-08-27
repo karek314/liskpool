9f848339aa82ea4fa4f93bd9eac05b120a2b4832
karek314 <karek314@protonmail.ch>
2017-08-27 22:59:08 +0200

    payout only for good nodes
    payout only for good nodes == running latest lisk version

--- private/liskstats.php
+++ private/liskstats.php
@@ -5,32 +5,63 @@ require_once('logging.php');
 require('wss/Client.php');
 use WebSocket\Client;
 
-$timestamp_ms = time()*1000;
-$client = new Client("ws://liskstats.net:3000/primus/?_primuscb=".$timestamp_ms."-0");
-$client->send('{"emit":["ready"]}');
-$x=0;
-$mysqli=mysqli_connect($config['host'], $config['username'], $config['password'], $config['bdd']) or die(mysqli_error($mysqli));
+$i=0;
 while (1) {
-  if (!$client->isConnected()) {
-    $client = new Client("ws://liskstats.net:3000/primus/?_primuscb=".$timestamp_ms."-0");
-    $client->send('{"emit":["ready"]}');
-  }
-  if ($x > 3600) {
-    clog("Cleaning everything (once per hour)",'liskstats');
-    $task = "TRUNCATE TABLE `liskstats`";
-    $query = mysqli_query($mysqli,$task) or die(mysqli_error($mysqli));
-    $x=0;
-  }
+  $i++;
+  $timestamp_ms = time()*1000;
+  $client = new Client("ws://liskstats.net:3000/primus/?_primuscb=".$timestamp_ms."-0");
+  $client->send('{"emit":["ready"]}');
+
+  clog("[".$i."]Cleaning everything",'liskstats');
+  $mysqli=mysqli_connect($config['host'], $config['username'], $config['password'], $config['bdd']) or die(mysqli_error($mysqli));
+  $task = "TRUNCATE TABLE `liskstats`";
+  $query = mysqli_query($mysqli,$task) or die(mysqli_error($mysqli));
+
   $response = json_decode($client->receive(),true);
-  if (isset($response['data'])) {
-    if (isset($response['data']['id'])) { 
-      $object = $response['data']['id'];
-      clog($object,'liskstats');
-      $task = "INSERT INTO liskstats (object) SELECT * FROM (SELECT '$object') AS tmp WHERE NOT EXISTS (SELECT * FROM liskstats WHERE object = '$object' LIMIT 1)";
-      $query = mysqli_query($mysqli,$task) or die(mysqli_error($mysqli));
+  $node_list = array();
+  $latest_lisk_version = "";
+  if (isset($response['emit'])) {
+    if (isset($response['emit'][1])) { 
+      if (isset($response['emit'][1]['nodes'])) { 
+        $array_of_nodes = $response['emit'][1]['nodes'];
+        foreach ($array_of_nodes as $key => $node) {
+          if (isset($node['info'])) {
+            if (isset($node['info']['protocol'])) {
+              $object = $node["id"];
+              $version = $node["info"]["protocol"];
+              $tmp = array('object' => $object, 'version' => $version);
+              $node_list[] = $tmp;
+            }
+          }
+        }
+      }
     }
   }
-  sleep(1);
+  foreach ($node_list as $key => $node) {
+    $object = $node["object"];
+    $version = $node["version"];
+    if (strtolower($object) == strtolower("thePoolIo")) {
+      $latest_lisk_version = $version;
+    }
+  }
+  clog("[".$i."]Latest version of Lisk:".$latest_lisk_version,'liskstats');
+  $ok = 0;
+  $all = 0;
+  foreach ($node_list as $key => $node) {
+    $all++;
+    $object = $node["object"];
+    $version = $node["version"];
+    if (strtolower($version) == strtolower($latest_lisk_version)) {
+      clog("[".$i."][OK]".$object."->".$version." RUNNING LATEST VERSION",'liskstats');
+      $ok++;
+      $task = "INSERT INTO liskstats (object) SELECT * FROM (SELECT '$object') AS tmp WHERE NOT EXISTS (SELECT * FROM liskstats WHERE object = '$object' LIMIT 1)";
+      $query = mysqli_query($mysqli,$task) or die(mysqli_error($mysqli));
+    } else {
+      clog("[".$i."]".$object."->".$version." Running old version",'liskstats');
+    }
+  }
+  clog("[".$i."]Good nodes:".$ok."/".$all,'liskstats');
+  sleep(1600);
   $x++;
 }
 
